e318d7ccaeed6029f477fabd53f0778e1f59bfa7
thasitho <thasitho@student.wethinkcode.com>
2020-11-03 10:01:57 +0200

    hello world

---
+++ ex00/reviews.py
@@ -0,0 +1,29 @@
+dsamuels7206: wtc-basics_grading : wtc-lms review_details 74a0184e-3271-41fb-8018-8353cfb4dffb
+lomcmast: Hangman - Iteration 1: wtc-lms review_details ed27cc65-1b10-4fdb-b7d9-6bdf7bb6e8da
+llndhlov: Hangman - Iteration 1: wtc-lms review_details 2bfb9e11-e782-4e11-8db6-439d4cad6c86
+bhmlange: Hangman - Iteration 1: wtc-lms review_details cbd46120-a2c5-4536-8833-7388bece77d5
+bidaniel: Hangman - Iteration 2: wtc-lms review_details a29c5bff-51c8-48b8-952d-7df033839a77
+cessack: Hangman - Iteration 2: wtc-lms review_details a627492d-ed40-44fa-a3ab-254d7ca9bd31
+ejohns: Hangman - Iteration 2 wtc-lms review_details 59f1f7ab-f33b-44c6-aceb-94f164853eba
+taismail: Hangman - Iteration 3: wtc-lms review_details 88958a72-191d-4196-bf91-4e82632bff51
+bidaniel: Problem - Pyramid: wtc-lms review_details 4371aab2-47f4-4e1b-8c0b-58a2146e0bed
+cjacobs: Problem - Pyramid: wtc-lms review_details e883eba7-c270-4180-8dfd-9022e02dbc18
+gfortune: Problem - Pyramid: wtc-lms review_details 2f57eb59-b53c-4b28-8c89-571a49173026
+dmorison: Hangman - Iteration 3: wtc-lms review_details 879d8138-798a-4e85-8e84-69374eb95273
+ejohns: Hangman - Iteration 3: wtc-lms review_details 56505072-f0f5-47ec-8e5c-ce0cb4a4b97b
+omoima: Problem - Outline: wtc-lms review_details 5df38b15-7903-4181-bd91-9fa635b358a3
+nddlamin: Problem - Outline: wtc-lms review_details c1b8b858-a1d8-4de0-a1a5-e9b6d5756cd3
+wmagagu: Problem - Outline: lwtc-lms review_details 5c2c25f1-f40c-4673-a1a6-f80f6905ad38
+cakonyan: Mastermind - Iteration 1: wtc-lms review_details 3b8459ef-6bb8-4bb4-9074-fa5c94ce0678
+jkokot: Mastermind - Iteration 1: wtc-lms review_details 376ae924-0c05-4ea2-8761-1d6f3cb7458b
+kmohlabu: Mastermind - Iteration 1: wtc-lms review_details 29060a1d-4829-4e95-9f16-b8ed8e455660
+hkhan: Toy Robot - Iteration 1: wtc-lms review_details 060e01a3-7484-4f15-a3ed-db5395dde4c3
+cskosana: Mastermind - Iteration 2: wtc-lms review_details 491ff853-7977-4dd1-af54-03886f7a5f37
+gfortune: Mastermind - Iteration 2: wtc-lms review_details 7a8999c7-7969-4e8e-a22f-86b53f8d12d8
+msegal: Mastermind - Iteration 2: wtc-lms review_details d7864f54-b539-4979-9029-9c300c6900d7
+
+
+
+
+
+
